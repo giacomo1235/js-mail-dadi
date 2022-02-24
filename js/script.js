bfde736a96599ce15ae521c875585ded5826298c
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-24 15:43:08 +0100

    aggiunto punteggi

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function lanciaDadi() {
 
         let randomNumber1 = Math.floor(Math.random() * 6) + 1;
         let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+        let punteggioGiacomo = 0;
+        let punteggioMacchina = 0;
 
         document.querySelector(".img1").setAttribute("src",
             "img/dice" + randomNumber1 + ".png");
@@ -15,10 +17,13 @@ function lanciaDadi() {
 
         else if (randomNumber1 < randomNumber2) {
             document.querySelector("h1").innerHTML = "Che Sfiga!";
+            punteggioMacchina+= 1;
         }
 
         else {
             document.querySelector("h1").innerHTML = "Sono il Migliore!";
+            punteggioGiacomo+=1;
         }
-    ;
+        document.getElementById("pg").innerHTML = punteggioGiacomo;
+        document.getElementById("pm").innerHTML = punteggioMacchina;
 }
