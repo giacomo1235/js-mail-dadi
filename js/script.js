6c4e7c990fd18c6d66b4c5e573e595bed497731e
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-24 15:31:50 +0100

    il gioco dei dadi funziona, devo aggiungere conteggio punti

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function lanciaDadi() {
+
+        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        document.querySelector(".img1").setAttribute("src",
+            "img/dice" + randomNumber1 + ".png");
+
+        document.querySelector(".img2").setAttribute("src",
+            "img/dice" + randomNumber2 + ".png");
+
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector("h1").innerHTML = "Patta!";
+        }
+
+        else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML = "Che Sfiga!";
+        }
+
+        else {
+            document.querySelector("h1").innerHTML = "Sono il Migliore!";
+        }
+    ;
+}
