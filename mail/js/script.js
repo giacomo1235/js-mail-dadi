9c4e340fcde2b569991c3be916aa1089c740e369
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-25 11:13:38 +0100

    indentazione

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,14 +1,25 @@
-const forbiddenMail = ["pietro.pacciani@gmail.com", "annamaria.franzoni@live.it", "amanda.knox@yahoo.us", "leonardacianciulli@live.com","andreadipre@gmail.com"];
+const forbiddenMail = [
+    "pietro.pacciani@gmail.com",
+    "annamaria.franzoni@live.it",
+    "amanda.knox@yahoo.us",
+    "leonardacianciulli@live.com",
+    "andreadipre@gmail.com"
+];
+
 let btnCerca = document.getElementById('cerca');
+
 const userMail = document.getElementById('inputMail').value;
+
 const eleOutput = document.getElementById('testo');
 
 btnCerca.addEventListener ('click', function() {    
+    
     let found = false;
+
     for (let i = 0; i < forbiddenMail.length; i++){
-    if (forbiddenMail[i] == userMail) {
-        found = true;
-    }
+        if (forbiddenMail[i] == userMail) {
+            found = true;
+        }
     };
     
     if (found) {
