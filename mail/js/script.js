6cae5cd212387790eadc8f7bb84ae7cca6867540
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-25 10:06:59 +0100

    non funziona

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,15 +1,18 @@
 const forbiddenMail = ["pietro.pacciani@gmail.com", "annamaria.franzoni@live.it", "amanda.knox@yahoo.us", "leonardacianciulli@live.com","andreadipre@gmail.com"];
 let btnCerca = document.getElementById('cerca');
-let userMail = document.getElementById('inputMail').value;
+const userMail = document.getElementById('inputMail').value;
+let found = false;
 
-btnCerca.addEventListener ('click', function() {
-    
-    
+btnCerca.addEventListener ('click', function() {    
     for (let i = 0; i < forbiddenMail.length; i++){
     if (userMail == forbiddenMail[i]) {
-        document.getElementById('testo').innerHTML = "invalido"
-    } else { 
-        document.getElementById('testo').innerHTML = "valido"
+        found = true;
     }
     };
+    
+    if (found) {
+        console.log('trovato')
+    } else {
+        console.log(' non trovato')
+    }
 });
